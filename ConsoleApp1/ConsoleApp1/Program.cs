3a8e37a37b17dd124d05b8b085c9678a56b9d4ec
Sasha-Gill <54946194+Sasha-Gill@users.noreply.github.com>
2019-10-18 15:48:08 +0200

    klar

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fighter A = new fighter();//skapar en ny fighter
+            fighter B = new fighter();//skapar en till
+
+            Console.WriteLine("NAME FOR FIGHTER A:");
+
+            A.Name = Console.ReadLine();//det man skriver in blir nmanet på A
+            Console.WriteLine("FIGHTER A's NAME IS NOW: "+A.Name+".");
+            Console.ReadLine();
+
+            Console.WriteLine("NAME FOR FIGHTER B:");
+
+            B.Name = Console.ReadLine();//det man skriver in blir nmanet på A
+            Console.WriteLine("FIGHTER B's NAME IS NOW: " + B.Name + ".");
+            Console.ReadLine();
+            Console.Clear();
+
+            while (A.IsAlive() && B.IsAlive())//så länge a och b lever så körs loopen
+            {
+                B.Hurt(A.Attack());//B.Hurt tar in A.attacks int amount som dess skada tagen
+                A.Hurt(B.Attack());//Samma sak fast tvärt om
+
+                Console.WriteLine(A.Name+" HP är: "+A.GetHp());//skriver ut det uppdaterade HP:t
+                Console.WriteLine(B.Name + " HP är: "+B.GetHp());//skriver ut det uppdaterade HP:t
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            if (A.IsAlive()==false)//Ifall A icke lever så vinner B
+            {
+                Console.WriteLine("CONGRATULATIONS "+B.Name+"!"+
+                    " YOU WON!");
+                Console.ReadLine();
+            }
+            else//Om a lever & B inte så körs denna
+            {
+                Console.WriteLine("CONGRATULATIONS " + A.Name +"!" +
+                    " YOU WON!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/fighter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class fighter
+    {
+        private int hp = 100;//Gör int:en privat så att man inte kan ändra HP värdet.
+        public string Name;//
+        private int minDmg = 10;//minsta mängden en fighter gör
+        private int maxDmg = 20;//största mängden en fighter gör
+        static Random generator = new Random();//Denna random gör jag till static så att alla använder sig utav samma generator.
+    
+        public int Attack()
+        {
+            int amount = generator.Next(minDmg, maxDmg);//amount är mängden damage dom gör
+            return amount;//returnerar amount
+        }
+        
+        public void Hurt(int amount)
+        {
+            hp -= amount;//hp blir hp - mängden damage.
+        }
+
+        public bool IsAlive()
+        {
+            return hp > 0;//den returnerar så länge hp är större än 0.
+        }
+
+        public int GetHp()
+        {
+            return hp;//Den returnerar det nuvarande HP som kämpen har.
+        }
+    }
+}
